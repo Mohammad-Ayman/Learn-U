d8c3faa24ea26f6df0667285bcb3e84a557f2fe9
Mohamed Ibrahim <mohdayman22rr@gmail.com>
2023-09-30 12:03:29 +0300

    ADD: AuthContext

--- Components/HomePage/MyLearning/MyLearningElement.jsx
+++ Components/HomePage/MyLearning/MyLearningElement.jsx
@@ -2,7 +2,6 @@ import styles from "./styles/myLearningElement.module.css";
 
 const MyLearningElement = (props) => {
   return (
-    <>
       <li
         className={`${styles["myLearning-card"]} mflex`}
         data-courseid={props.id}
@@ -24,6 +23,7 @@ const MyLearningElement = (props) => {
                   fillRule="evenodd"
                   d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
                   clipRule="evenodd"
+                  
                 />
               </svg>
             </div>
@@ -32,7 +32,6 @@ const MyLearningElement = (props) => {
           {props.children}
         </div>
       </li>
-    </>
   );
 };
 
