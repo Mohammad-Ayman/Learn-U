3ff0ac6d561b4d55f4ddf4c8fb43cd6b14623f1b
Mohamed Ibrahim <mohdayman22rr@gmail.com>
2023-12-03 16:13:06 +0300

    FEAT: loading page

--- Components/HomePage/FeaturedCourses/FeaturedCourses.jsx
+++ Components/HomePage/FeaturedCourses/FeaturedCourses.jsx
@@ -3,14 +3,17 @@ import React, { useState, useEffect } from "react";
 import DisplayCourses from "./DisplayCourses";
 import styles from "./styles/featuredCourses.module.css";
 import { fetchLimitedCourses } from "@/Components/Fetching/fetching";
-import { courses } from "@/app/page";
+import LoadingPage from "@/Components/UI/LoadingPage";
 
 const FeaturedCourses = () => {
   const [featuredCourses, setFeaturedCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     const fetchFeaturedCourses = async () => {
       const courses = await fetchLimitedCourses(5);
       setFeaturedCourses(courses);
+      setIsLoading(false);
     };
 
     fetchFeaturedCourses();
@@ -18,7 +21,11 @@ const FeaturedCourses = () => {
   return (
     <section className={styles["features-container"]}>
       <h2 className="header-text">Featured Courses</h2>
-      <DisplayCourses AllCourses={featuredCourses} />
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <DisplayCourses AllCourses={featuredCourses} />
+      )}
     </section>
   );
 };
